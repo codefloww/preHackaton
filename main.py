d149c87aa7060e36c52f201bb59a59ac538b5325
codefloww <kryvpaul@gmail.com>
2021-10-25 14:07:28 +0300

    added modules for the game

---
+++ main.py
@@ -0,0 +1,24 @@
+"""Main module of the game that supports all other modules
+#menu
+    #start game
+        #draw a window 
+        #start generating obsticles'
+        #end of the game
+            #score
+            #play again
+            #go into main menu
+    #highscores
+        #display list of highscores
+    #exit
+    
+
+"""
+import pygame
+import main_menu
+import game
+import end_menu
+import highscores
+
+
+def main():
+    
